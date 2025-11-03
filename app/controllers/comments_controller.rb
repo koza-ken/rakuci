69d3d9af9cd26f6eda6de5d645d360cb2325d413
kozaken <gkozaka4@gmail.com>
2025-11-03 20:15:18 +0900

    add:commentsコントローラにフィルターとそのメソッドを追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,9 @@
 class CommentsController < ApplicationController
+  before_action :set_card
+  before_action :set_comment, only: [:destroy]
+  before_action :check_comment_owner, only: [:destroy]
+
   def create
-    @card = Card.find(params[:card_id])
     @comment = @card.comments.build(comment_params)
     @comment.group_membership = current_group_membership_for(@card.group_id)
     if @comment.save
@@ -25,6 +28,26 @@ class CommentsController < ApplicationController
   end
 
   private
+
+  def set_card
+    @card = Card.find(params[:card_id])
+  end
+
+  def set_comment
+    @comment = @card.comments.find(params[:id])
+  end
+
+  def check_comment_owner
+    current_membership = current_group_membership_for(@card.group_id)
+    unless current_membership && current_membership.id == @comment.group_membership_id
+      respond_to do |format|
+        format.turbo_stream {
+          redirect_to card_path(@card), alert: "削除する権限がありません"
+        }
+        format.html { redirect_to card_path(@card), alert: "削除する権限がありません" }
+      end
+    end
+  end
 
   def comment_params
     params.require(:comment).permit(:content)
