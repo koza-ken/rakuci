df6809f5ea8bbed58b846ad5225c34d3b5f2ceb9
kozaken <gkozaka4@gmail.com>
2025-11-05 21:31:37 +0900

    add:schedule_spotsのマイグレーションファイルを実行しテーブルに反映

--- test/models/schedule_spot_test.rb
+++ test/models/schedule_spot_test.rb
@@ -1,3 +1,35 @@
+# == Schema Information
+#
+# Table name: schedule_spots
+#
+#  id                    :bigint           not null, primary key
+#  day_number            :integer          not null
+#  end_time              :time
+#  global_position       :integer          not null
+#  is_custom_entry       :boolean          default(FALSE), not null
+#  memo                  :text
+#  schedulable_type      :string           not null
+#  snapshot_address      :string
+#  snapshot_name         :string
+#  snapshot_phone_number :string
+#  snapshot_website_url  :string
+#  start_time            :time
+#  created_at            :datetime         not null
+#  updated_at            :datetime         not null
+#  schedulable_id        :bigint           not null
+#  snapshot_category_id  :integer
+#  spot_id               :bigint           not null
+#
+# Indexes
+#
+#  index_schedule_spots_on_spot_id       (spot_id)
+#  index_ss_on_schedulable_and_day       (schedulable_type,schedulable_id,day_number)
+#  index_ss_on_schedulable_and_position  (schedulable_type,schedulable_id,global_position) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (spot_id => spots.id)
+#
 require "test_helper"
 
 class ScheduleSpotTest < ActiveSupport::TestCase
