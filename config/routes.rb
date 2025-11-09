fbf7c3fa1978b2e76fd94ea46f51653a77956231
kozaken <gkozaka4@gmail.com>
2025-11-09 17:05:35 +0900

    add:グループ用しおりにスポット追加するルートとしおり詳細ページにアクセスするルートを追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
     resources :spots, only: %i[show new create edit update destroy] do
       # 個人用しおりのスポット追加
       resources :schedule_spots, only: %i[new create], controller: 'users/schedule_spots', path: 'user_schedule_spot_path'
+      # グループ用しおりのスポット追加（作成ページなし）
+      post "/group_schedule_spots", to: "groups/schedule_spots#create", as: :group_schedule_spot
+    end
     resources :comments, only: %i[create destroy]
     resource :likes, only: %i[create destroy]
   end
@@ -23,6 +26,13 @@ Rails.application.routes.draw do
     end
   end
 
+  # URLは"/schedules"
+  scope module: "groups" do
+    resources :schedules, only: %i[show] do
+      # showはscheduleの詳細からアクセスする（追加のnew,createは/card/spotsから）
+      resources :schedule_spots, only: %i[show],  as: 'group_schedule_spot', path: '/group_schedule_spots'
+    end
+  end
 
   # 招待リンクからの参加
   # asオプションで、/groups/join/:invite_tokenのURLを生成するヘルパーを定義
