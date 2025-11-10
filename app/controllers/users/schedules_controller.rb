eef75176e8caf42d0457f7e0dde6eeefe5b61d5f
kozaken <gkozaka4@gmail.com>
2025-11-10 22:13:29 +0900

    add:しおり一覧ページのindexアクションで個人用とグループ用のしおりを取得する

--- app/controllers/users/schedules_controller.rb
+++ app/controllers/users/schedules_controller.rb
@@ -1,9 +1,11 @@
 class Users::SchedulesController < ApplicationController
-before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def index
     @user = current_user
-    @schedules = @user.schedules
+    personal_schedules = @user.schedules
+    group_schedules = @user.groups.map(&:schedule).compact
+    @schedules = (personal_schedules + group_schedules).sort_by { |s| s.start_date.presence || Date.new(1, 1, 1) }.reverse
   end
 
   def show
