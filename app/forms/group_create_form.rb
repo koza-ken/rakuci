293b8b6c5b3f06a1e293f8a949d96ee58172adcd
kozaken <gkozaka4@gmail.com>
2025-11-11 21:38:58 +0900

    fix:groupsテーブルから削除したカラムを、グループ作成フォームから削除

--- app/forms/group_create_form.rb
+++ app/forms/group_create_form.rb
@@ -9,9 +9,6 @@ class GroupCreateForm
 
   # groupsモデル
   attribute :name, :string
-  attribute :trip_name, :string
-  attribute :start_date, :date
-  attribute :end_date, :date
   # group_membershipモデル
   attribute :group_nickname, :string
   attribute :role, :string
@@ -24,10 +21,7 @@ class GroupCreateForm
 
     ActiveRecord::Base.transaction do
       @group = user.created_groups.build(
-        name: name,
-        trip_name: trip_name,
-        start_date: start_date,
-        end_date: end_date
+        name: name
       )
       @group.group_memberships.build(
         user_id: user.id,
