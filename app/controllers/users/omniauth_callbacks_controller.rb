f92a82679b672b7467730cb1f9412bab970c1921
kozaken <gkozaka4@gmail.com>
2025-12-02 19:54:19 +0900

    add:認証のコントローラとアクションを定義

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,26 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  skip_before_action :verify_authenticity_token, only: :google_oauth2
+
+  def google_oauth2
+    handle_callback(:google_oauth2, "Google")
+  end
+
+  def failure
+    redirect_to root_path
+  end
+
+  private
+
+  def handle_callback(provider_key, provider_name)
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: provider_name) if is_navigational_format?
+    else
+      session["devise.#{provider_key}_data"] = request.env["omniauth.auth"].except(:extra)
+      flash[:alert] = "#{provider_name}ログインに失敗しました。もう一度お試しください。"
+      redirect_to new_user_registration_url
+    end
+  end
+end
