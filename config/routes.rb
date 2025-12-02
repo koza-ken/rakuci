97ca124a7d5760c01d59aaa87e2d4bb31ecadd15
kozaken <gkozaka4@gmail.com>
2025-12-02 19:53:59 +0900

    add:認証のルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root "static_pages#home"
 
   resources :cards, only: %i[index show new create update destroy] do
