f68f90accdf6c056bb01f0e995206533bfe5c2e7
kozaken <gkozaka4@gmail.com>
2025-12-09 14:55:11 +0900

    refactor:spotsコントローラの個人カード部分をusersで切り出し

---
+++ app/controllers/users/spots_controller.rb
@@ -0,0 +1,61 @@
+class Users::SpotsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_card
+  before_action :set_spot, only: %i[show edit update destroy]
+
+  def show
+  end
+
+  def new
+    @spot = @card.spots.build
+    @categories = Category.all.order(:display_order)
+  end
+
+  def create
+    @spot = @card.spots.build(spot_params)
+    # 空文字列のgoogle_place_idをnilに変換（データベースのユニーク制約対策）
+    @spot.google_place_id = nil if @spot.google_place_id.blank?
+
+    if @spot.save
+      respond_to do |format|
+        format.turbo_stream { flash.now[:notice] = t("notices.spots.created") }
+        format.html { redirect_to card_path(@card), notice: t("notices.spots.created") }
+      end
+    else
+      @categories = Category.all.order(:display_order)
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @categories = Category.all.order(:display_order)
+  end
+
+  def update
+    if @spot.update(spot_params)
+      redirect_to card_spot_path(@card, @spot), notice: t("notices.spots.updated")
+    else
+      @categories = Category.all.order(:display_order)
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @spot.destroy!
+    redirect_to card_path(@card), notice: t("notices.spots.destroyed")
+  end
+
+  private
+
+  def set_card
+    @card = current_user.cards.find(params[:card_id])
+  end
+
+  def set_spot
+    @spot = @card.spots.find(params[:id])
+  end
+
+  def spot_params
+    params.require(:spot).permit(:name, :address, :phone_number, :website_url, :category_id, :google_place_id)
+  end
+end
