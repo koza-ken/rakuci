973e444ac8bee8131e3b79171e886eb451495cc4
kozaken <gkozaka4@gmail.com>
2025-12-20 07:43:37 +0900

    add:itemモデルに関連付けやバリデーションを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  belongs_to :item_list
+
+  # リストごとに並び順を管理するためにスコープを設定
+  acts_as_list scope: :item_list
+
+  validates :name, presence: true, length: { maximum: 100 }
 end
