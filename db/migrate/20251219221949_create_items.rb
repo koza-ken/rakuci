1803fa56fa6b26be71d20ccef4e93706fd7468c2
kozaken <gkozaka4@gmail.com>
2025-12-20 07:38:18 +0900

    add:itemsテーブルのマイグレーションファイルに制約追加

--- db/migrate/20251219221949_create_items.rb
+++ db/migrate/20251219221949_create_items.rb
@@ -1,12 +1,16 @@
 class CreateItems < ActiveRecord::Migration[7.2]
   def change
     create_table :items do |t|
-      t.integer :item_list_id
-      t.string :name
-      t.boolean :checked
+      t.integer :item_list_id, null: false
+      t.string :name, limit: 100, null: false
+      t.boolean :checked, null: false, default: false
       t.integer :position
 
       t.timestamps
     end
+
+    add_index :items, :item_list_id, name: "index_items_on_item_list_id"
+    add_index :items, [:item_list_id, :position], name: "index_items_on_list_and_position"
+    add_foreign_key :items, :item_lists
   end
 end
