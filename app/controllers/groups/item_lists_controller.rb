b21a262b3120c6a88e1d0e73fae78ae0d39469f4
kozaken <gkozaka4@gmail.com>
2025-12-20 13:33:21 +0900

    add:もちものリストともちもののコントローラ（グループ用）を追加

---
+++ app/controllers/groups/item_lists_controller.rb
@@ -0,0 +1,38 @@
+module Groups
+  class ItemListsController < ApplicationController
+    before_action :set_group
+    before_action :check_group_member
+    before_action :set_schedule
+    before_action :set_item_list, only: %i[show]
+
+    # GET /groups/:group_id/schedule/item_list
+    def show
+    end
+
+    private
+
+    def set_group
+      @group = Group.find(params[:group_id])
+    end
+
+    def check_group_member
+      authorized = if user_signed_in?
+        current_user.member_of?(@group)
+      else
+        GroupMembership.guest_member?(guest_token_for(@group.id), @group.id)
+      end
+
+      unless authorized
+        redirect_to (user_signed_in? ? groups_path : root_path), alert: t("errors.groups.not_member")
+      end
+    end
+
+    def set_schedule
+      @schedule = @group.schedule
+    end
+
+    def set_item_list
+      @item_list = @schedule.item_list
+    end
+  end
+end
