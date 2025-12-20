1ab52f3ce4f35daecde41b20d2097e0babe2851e
kozaken <gkozaka4@gmail.com>
2025-12-20 09:52:59 +0900

    fix:item-listモデルのバリデーションからinclusionを削除

--- app/models/item_list.rb
+++ app/models/item_list.rb
@@ -21,7 +21,7 @@ class ItemList < ApplicationRecord
   has_many :items, dependent: :destroy
 
   # バリデーション
-  validates :listable_type, presence: true, inclusion: { in: %w[User Schedule] }
+  validates :listable_type, presence: true
   validates :listable_id, presence: true
   validates :name, length: { maximum: 100 }
 end
