0025bde62f641aadeb796a46c7947580fb963155
kozaken <gkozaka4@gmail.com>
2026-01-09 19:35:45 +0900

    fix:nameの変更のあとに保存が走ってカスタムバリデーションがその後に検証されるようになっていたのを、すべての検証の後に保存されるよう修正する

--- app/controllers/groups/expenses_controller.rb
+++ app/controllers/groups/expenses_controller.rb
@@ -47,9 +47,17 @@ class Groups::ExpensesController < ApplicationController
   def update
     participant_ids = params[:expense][:participant_ids]&.reject(&:blank?) || []
 
-    if @expense.update(expense_params)
-      # 参加者を更新
-      @expense.expense_participants.destroy_all
+    # 既存参加者を削除してから新しい参加者をbuild
+    @expense.expense_participants.destroy_all
+    participant_ids.each do |participant_id|
+      @expense.expense_participants.build(group_membership_id: participant_id)
+    end
+
+    # 属性を割り当て
+    @expense.attributes = expense_params
+
+    if @expense.save
+      # 参加者を永続化（既存は destroy_all で削除済み）
       participant_ids.each do |participant_id|
         @expense.expense_participants.create(group_membership_id: participant_id)
       end
