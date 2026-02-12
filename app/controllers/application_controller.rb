76d1a87b1cd9166b5a602f7232539c312823aa52
kozaken <gkozaka4@gmail.com>
2026-02-12 09:15:47 +0900

    remove:コメントアウトして残していたけどいらなさそうなので削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,4 @@ class ApplicationController < ActionController::Base
   include GuestAuthentication
   include FriendlyForwarding
 
-  # ログイン後のリダイレクト先を設定（resouceを渡すとユーザーの属性で判別できる）
-  # def after_sign_in_path_for(resource)
-  #   store_location_for(:user, request.fullpath) || cards_path
-  # end
 end
