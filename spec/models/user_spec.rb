81e13d2d0b2afb4769901fb70d498fd29dd2ecf1
kozaken <gkozaka4@gmail.com>
2026-02-13 21:31:19 +0900

    add:追加したメソッドのテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -94,6 +94,75 @@ RSpec.describe User, type: :model do
     end
   end
 
+  describe "#oauth_user?" do
+    context "provider と uid の両方がある場合" do
+      it "trueを返すこと" do
+        user = build(:user, :oauth)
+        expect(user.oauth_user?).to be(true)
+      end
+    end
+
+    context "provider のみある場合" do
+      it "falseを返すこと" do
+        user = build(:user, provider: "google_oauth2", uid: nil)
+        expect(user.oauth_user?).to be(false)
+      end
+    end
+
+    context "uid のみある場合" do
+      it "falseを返すこと" do
+        user = build(:user, provider: nil, uid: "123")
+        expect(user.oauth_user?).to be(false)
+      end
+    end
+
+    context "両方ない場合（通常のメール登録ユーザー）" do
+      it "falseを返すこと" do
+        user = build(:user)
+        expect(user.oauth_user?).to be(false)
+      end
+    end
+  end
+
+  describe "コールバック" do
+    describe "after_create :create_item_list" do
+      it "ユーザー作成時に ItemList が生成されること" do
+        expect { create(:user) }.to change(ItemList, :count).by(1)
+      end
+
+      it "生成された ItemList がユーザーに紐づいていること" do
+        user = create(:user)
+        expect(user.item_list).to be_present
+        expect(user.item_list.listable).to eq(user)
+      end
+    end
+  end
+
+  describe "#cards_with_spots_grouped" do
+    it "カードとスポットをカテゴリIDでグループ化して返すこと" do
+      user = create(:user)
+      card = create(:card, cardable: user, name: "京都旅行")
+      category1 = create(:category)
+      category2 = create(:category)
+      create(:spot, card: card, category: category1, name: "金閣寺")
+      create(:spot, card: card, category: category2, name: "抹茶カフェ")
+
+      result = user.cards_with_spots_grouped
+
+      expect(result.length).to eq(1)
+      grouped_card, spots_by_category = result.first
+      expect(grouped_card).to eq(card)
+      expect(spots_by_category.keys).to contain_exactly(category1.id, category2.id)
+      expect(spots_by_category[category1.id].map(&:name)).to eq([ "金閣寺" ])
+      expect(spots_by_category[category2.id].map(&:name)).to eq([ "抹茶カフェ" ])
+    end
+
+    it "カードがない場合は空配列を返すこと" do
+      user = create(:user)
+      expect(user.cards_with_spots_grouped).to eq([])
+    end
+  end
+
   describe "member_ofメソッド" do
     context "ユーザーがグループのメンバーの場合" do
       it "member_ofはtrueを返すこと" do
