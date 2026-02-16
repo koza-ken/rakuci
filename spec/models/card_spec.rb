ad3910f84e7d3a91e3dc25700394db91efacdd63
kozaken <gkozaka4@gmail.com>
2026-02-16 11:05:07 +0900

    refactor:一部のメソッドをprivateにしたことで、直接メソッドを呼ばないテストに修正

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -62,119 +62,67 @@ RSpec.describe Card, type: :model do
   end
 
   describe "メソッド" do
-    describe "#accessible_by_user?メソッド" do
-      context "グループカードの場合" do
-        context "ユーザーがグループメンバーの場合" do
-          it "trueを返すこと" do
+    describe "#accessible?メソッド" do
+      context "ログインユーザーの場合" do
+        context "グループカードの場合" do
+          it "グループメンバーならtrueを返すこと" do
             user = create(:user)
             group = create(:group)
             create(:group_membership, user: user, group: group)
             card = create(:card, :for_group, cardable: group)
 
-            expect(card.accessible_by_user?(user)).to be true
+            expect(card.accessible?(user: user, guest_group_ids: [])).to be true
           end
-        end
 
-        context "ユーザーがグループメンバーではない場合" do
-          it "falseを返すこと" do
+          it "グループメンバーでなければfalseを返すこと" do
             user = create(:user)
-            group = create(:group)
             card = create(:card, :for_group)
 
-            expect(card.accessible_by_user?(user)).to be false
+            expect(card.accessible?(user: user, guest_group_ids: [])).to be false
           end
         end
-      end
 
-      context "個人カードの場合" do
-        context "ユーザーが作成者の場合" do
-          it "trueを返すこと" do
+        context "個人カードの場合" do
+          it "作成者ならtrueを返すこと" do
             user = create(:user)
             card = create(:card, :for_user, cardable: user)
 
-            expect(card.accessible_by_user?(user)).to be true
+            expect(card.accessible?(user: user, guest_group_ids: [])).to be true
           end
-        end
 
-        context "ユーザーが作成者ではない場合" do
-          it "falseを返すこと" do
+          it "作成者でなければfalseを返すこと" do
             user1 = create(:user)
             user2 = create(:user)
             card = create(:card, :for_user, cardable: user1)
 
-            expect(card.accessible_by_user?(user2)).to be false
+            expect(card.accessible?(user: user2, guest_group_ids: [])).to be false
           end
         end
       end
-    end
 
-    describe "#accessible_by_guest?メソッド" do
-      context "個人カードの場合" do
-        it "falseを返すこと" do
-          card = create(:card, :for_user)
-          expect(card.accessible_by_guest?([ 1, 2, 3 ])).to be false
-        end
-      end
-
-      context "グループカードの場合" do
-        context "ゲストユーザーが参加できるグループidの配列（guest_group_ids）にグループが含まれる場合" do
-          it "trueを返すこと" do
+      context "ゲストの場合" do
+        context "グループカードの場合" do
+          it "参加済みグループのカードならtrueを返すこと" do
             group = create(:group)
             card = create(:card, :for_group, cardable: group)
-            guest_group_ids = [ group.id, 2, 3 ]
 
-            expect(card.accessible_by_guest?(guest_group_ids)).to be true
+            expect(card.accessible?(user: nil, guest_group_ids: [ group.id ])).to be true
+          end
+
+          it "参加していないグループのカードならfalseを返すこと" do
+            group = create(:group)
+            card = create(:card, :for_group, cardable: group)
+
+            expect(card.accessible?(user: nil, guest_group_ids: [ 999 ])).to be false
           end
         end
 
-        context "ゲストユーザーが参加できるグループidの配列（guest_group_ids）にグループが含まれない場合" do
+        context "個人カードの場合" do
           it "falseを返すこと" do
-            group = create(:group)
-            card = create(:card, :for_group, cardable: group)
-            guest_group_ids = [ 999, 1000, 1001 ]
+            card = create(:card, :for_user)
 
-            expect(card.accessible_by_guest?(guest_group_ids)).to be false
+            expect(card.accessible?(user: nil, guest_group_ids: [ 1, 2, 3 ])).to be false
           end
-        end
-      end
-    end
-
-
-    describe "#accessible?メソッド" do
-      context "ログインユーザーがアクセスできる場合" do
-        it "trueを返すこと" do
-          user = create(:user)
-          card = create(:card, :for_user, cardable: user)
-
-          expect(card.accessible?(user: user, guest_group_ids: [])).to be true
-        end
-      end
-
-      context "ログインユーザーがアクセスできない場合" do
-        it "falseを返すこと" do
-          user1 = create(:user)
-          user2 = create(:user)
-          card = create(:card, :for_user, cardable: user1)
-
-          expect(card.accessible?(user: user2, guest_group_ids: [])).to be false
-        end
-      end
-
-      context "ゲストがアクセスできる場合" do
-        it "trueを返すこと" do
-          group = create(:group)
-          card = create(:card, :for_group, cardable: group)
-
-          expect(card.accessible?(user: nil, guest_group_ids: [ group.id ])).to be true
-        end
-      end
-
-      context "ゲストがアクセスできない場合" do
-        it "falseを返すこと" do
-          group = create(:group)
-          card = create(:card, :for_group, cardable: group)
-
-          expect(card.accessible?(user: nil, guest_group_ids: [ 999 ])).to be false
         end
       end
     end
