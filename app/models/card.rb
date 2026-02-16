2d540214dee18249d48696e83e6ad2d9768552a4
kozaken <gkozaka4@gmail.com>
2026-02-16 11:04:31 +0900

    refactor:cardモデルのメソッドをprivateに修正

--- app/models/card.rb
+++ app/models/card.rb
@@ -24,28 +24,6 @@ class Card < ApplicationRecord
   belongs_to :cardable, polymorphic: true, touch: true
   validates :name, presence: true, length: { maximum: 50 }
 
-  # カードのタイプを返す（個人用 or グループ用）
-  def card_type
-    cardable_type == "User" ? :personal : :group
-  end
-
-  # そのユーザーがカードにアクセス可能か
-  def accessible_by_user?(user)
-    if card_type == :group
-      # グループカード：グループメンバーのみ
-      user.member_of?(cardable)
-    else
-      # 個人カード：所有者のみ
-      cardable_id == user.id
-    end
-  end
-
-  # ゲストユーザーがカードにアクセス可能か
-  def accessible_by_guest?(guest_group_ids)
-    return false if card_type == :personal  # 個人カードは不可
-    guest_group_ids.include?(cardable_id)
-  end
-
   # 引数にuserがあれば、accessible_by_user?でカードにアクセス可能か確認
   #  userがなければ（ゲスト）、参加済みグループのidにカードのidが含まれるかを確認
   def accessible?(user:, guest_group_ids:)
@@ -74,5 +52,29 @@ class Card < ApplicationRecord
   def liked_by?(group_membership)
     return false unless group_membership
     likes.exists?(group_membership: group_membership)
+  end
+
+  private
+
+  # カードのタイプを返す（個人用 or グループ用）
+  def card_type
+    cardable_type == "User" ? :personal : :group
+  end
+
+  # そのユーザーがカードにアクセス可能か
+  def accessible_by_user?(user)
+    if card_type == :group
+      # グループカード：グループメンバーのみ
+      user.member_of?(cardable)
+    else
+      # 個人カード：所有者のみ
+      cardable_id == user.id
+    end
+  end
+
+  # ゲストユーザーがカードにアクセス可能か
+  def accessible_by_guest?(guest_group_ids)
+    return false if card_type == :personal  # 個人カードは不可
+    guest_group_ids.include?(cardable_id)
   end
 end
