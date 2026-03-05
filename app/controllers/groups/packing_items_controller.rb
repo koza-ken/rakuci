f65b1a763cd85bba7d0351769269cb1b73a46d5b
kozaken <gkozaka4@gmail.com>
2026-03-05 10:26:07 +0900

    refactor:UsersとGroupsでコントローラの処理をconcernに共通化

--- app/controllers/groups/packing_items_controller.rb
+++ app/controllers/groups/packing_items_controller.rb
@@ -1,62 +1,12 @@
 class Groups::PackingItemsController < ApplicationController
   include GroupMemberAuthorization  # グループメンバーのみアクセス許可
+  include PackingItemActions        # create, update, destroyアクションを提供
 
   before_action :set_group
   before_action :check_group_member
   before_action :set_schedule
   before_action :set_packing_list, only: %i[create update destroy]
-  before_action :set_packing_item, only: %i[update destroy]
-
-  # POST /groups/:group_id/schedule/packing_list/items
-  def create
-    @packing_item = @packing_list.packing_items.build(packing_item_params)
-
-    if @packing_item.save
-      @saved_item = @packing_item # リスト追加用に保存済みアイテムを保持
-      @packing_item = @packing_list.packing_items.build # フォームリセット用に新しい空のインスタンスを作成（newアクションの代わり）
-      respond_to do |format|
-        format.turbo_stream
-        format.html { redirect_to group_schedule_packing_list_path(@group) }
-      end
-    else
-      # エラー時は@packing_itemを上書きしない（エラーメッセージを保持）
-      respond_to do |format|
-        format.turbo_stream
-        format.html { redirect_to group_schedule_packing_list_path(@group), alert: t("errors.packing_items.create_failed") }
-      end
-    end
-  end
-
-  # PATCH/PUT /groups/:group_id/schedule/packing_list/items/:id
-  def update
-    if @packing_item.update(packing_item_params)
-      # checked, position だけの更新の場合 (AJAX-only)
-      if request.format.json? || (packing_item_params.key?(:checked) && !packing_item_params.key?(:name)) || (packing_item_params.key?(:position) && !packing_item_params.key?(:name))
-        head :ok
-      else
-        # 名前の更新の場合 (Turbo Stream)
-        respond_to do |format|
-          format.turbo_stream
-          format.html { redirect_to group_schedule_packing_list_path(@group) }
-        end
-      end
-    else
-      respond_to do |format|
-        format.turbo_stream
-        format.html { redirect_to group_schedule_packing_list_path(@group), alert: t("errors.packing_items.update_failed") }
-      end
-    end
-  end
-
-  # DELETE /groups/:group_id/schedule/packing_list/items/:id
-  def destroy
-    @packing_item.destroy
-
-    respond_to do |format|
-      format.turbo_stream
-      format.html { redirect_to group_schedule_packing_list_path(@group) }
-    end
-  end
+  before_action :set_packing_item, only: %i[update destroy]  # PackingItemActions
 
   private
 
@@ -72,11 +22,8 @@ class Groups::PackingItemsController < ApplicationController
     @packing_list = @schedule.packing_list
   end
 
-  def set_packing_item
-    @packing_item = @packing_list.packing_items.find(params[:id])
-  end
-
-  def packing_item_params
-    params.require(:packing_item).permit(:name, :checked, :position)
+  # concernの上書き
+  def redirect_path
+    group_schedule_packing_list_path(@group)
   end
 end
--- app/controllers/users/packing_items_controller.rb
+++ app/controllers/users/packing_items_controller.rb
@@ -1,60 +1,10 @@
 class Users::PackingItemsController < ApplicationController
+  include PackingItemActions  # create, update, destroyアクションを提供
+
   before_action :authenticate_user!
   before_action :set_schedule, only: %i[create update destroy], if: -> { params[:schedule_id].present? }
   before_action :set_packing_list, only: %i[create update destroy]
-  before_action :set_packing_item, only: %i[update destroy]
-
-  # POST /packing_list/items または /schedules/:schedule_id/packing_list/items
-  def create
-    @packing_item = @packing_list.packing_items.build(packing_item_params)
-    @form_path = determine_form_path # 常にパスを設定
-
-    if @packing_item.save
-      @saved_item = @packing_item # リスト追加用に保存済みアイテムを保持
-      @packing_item = @packing_list.packing_items.build # フォームリセット用に新しい空のインスタンスを作成（newアクションの代わり）
-      respond_to do |format|
-        format.turbo_stream
-        format.html { redirect_to packing_list_path, notice: t("notices.packing_items.created") }
-      end
-    else
-      respond_to do |format|
-        format.turbo_stream
-        format.html { redirect_to packing_list_path, alert: t("errors.packing_items.create_failed") }
-      end
-    end
-  end
-
-  # PATCH/PUT /packing_list/items/:id または /schedules/:schedule_id/packing_list/items/:id
-  def update
-    # checked, position フィールドのみの更新か、name フィールドの更新かで分岐
-    if @packing_item.update(packing_item_params)
-      # checked, position だけの更新の場合 (AJAX-only)
-      if request.format.json? || (packing_item_params.key?(:checked) && !packing_item_params.key?(:name)) || (packing_item_params.key?(:position) && !packing_item_params.key?(:name))
-        head :ok
-      else
-        # 名前の更新の場合 (Turbo Stream)
-        respond_to do |format|
-          format.turbo_stream
-          format.html { redirect_to packing_list_path, notice: t("notices.packing_items.updated") }
-        end
-      end
-    else
-      respond_to do |format|
-        format.turbo_stream
-        format.html { redirect_to packing_list_path, alert: t("errors.packing_items.update_failed") }
-      end
-    end
-  end
-
-  # DELETE /packing_list/items/:id または /schedules/:schedule_id/packing_list/items/:id
-  def destroy
-    @packing_item.destroy
-
-    respond_to do |format|
-      format.turbo_stream
-      format.html { redirect_to packing_list_path, notice: t("notices.packing_items.destroyed") }
-    end
-  end
+  before_action :set_packing_item, only: %i[update destroy]  # PackingItemActions
 
   private
 
@@ -70,19 +20,8 @@ class Users::PackingItemsController < ApplicationController
     end
   end
 
-  def set_packing_item
-    @packing_item = @packing_list.packing_items.find(params[:id])
-  end
-
-  def determine_form_path
-    if @packing_list.listable_type == "User"
-      packing_list_items_path
-    else
-      schedule_packing_list_items_path(@packing_list.listable)
-    end
-  end
-
-  def packing_item_params
-    params.require(:packing_item).permit(:name, :checked, :position)
+  # concernの上書き
+  def redirect_path
+    packing_list_path
   end
 end
