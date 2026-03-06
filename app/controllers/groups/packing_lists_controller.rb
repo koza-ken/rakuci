30f729294c07f70aae4521c2eb0d5bdb2ae07fd8
kozaken <gkozaka4@gmail.com>
2026-03-06 19:45:38 +0900

    add:ビューのロジックをコントローラにヘルパーメソッドとして定義

--- app/controllers/groups/packing_lists_controller.rb
+++ app/controllers/groups/packing_lists_controller.rb
@@ -6,6 +6,8 @@ class Groups::PackingListsController < ApplicationController
   before_action :set_schedule
   before_action :set_packing_list, only: %i[show]
 
+  helper_method :item_delete_path, :form_url, :back_path, :input_class
+
   # GET /groups/:group_id/schedule/packing_list
   def show
     @packing_items = @packing_list.packing_items.order(:position)
@@ -25,4 +27,22 @@ class Groups::PackingListsController < ApplicationController
   def set_packing_list
     @packing_list = @schedule.packing_list
   end
+
+  # ヘルパーメソッド
+  def item_delete_path(item)
+    group_schedule_packing_list_item_path(@group, item)
+  end
+
+  def form_url
+    group_schedule_packing_list_items_path(@group)
+  end
+
+  def back_path
+    group_schedule_path(@group)
+  end
+
+  def input_class
+    "flex-1"
+  end
+
 end
--- app/controllers/users/packing_lists_controller.rb
+++ app/controllers/users/packing_lists_controller.rb
@@ -3,6 +3,8 @@ class Users::PackingListsController < ApplicationController
   before_action :set_schedule, only: %i[show], if: -> { params[:schedule_id].present? }
   before_action :set_packing_list, only: %i[show]
 
+  helper_method :item_delete_path, :form_url, :back_path, :input_class
+
   # GET /packing_list または /schedules/:schedule_id/packing_list
   def show
     @packing_items = @packing_list.packing_items.order(:position)
@@ -21,5 +23,22 @@ class Users::PackingListsController < ApplicationController
     else
       current_user.packing_list
     end
+  end
+
+  # ヘルパーメソッド
+  def item_delete_path(item)
+    @schedule.present? ? schedule_packing_list_item_path(@schedule, item) : packing_list_item_path(item)
+  end
+
+  def form_url
+    @schedule.present? ? schedule_packing_list_items_path(@schedule) : packing_list_items_path
+  end
+
+  def back_path
+    @schedule.present? ? schedule_path(@schedule) : nil
+  end
+
+  def input_class
+    ""
   end
 end
